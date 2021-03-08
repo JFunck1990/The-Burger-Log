0bd101e713192b65da12c4ece16a478947c3d670
Jack Funck <jackfunck21@gmail.com>
2021-03-08 14:24:00 -0500

    db connection fix

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,9 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
-
+let connection;
+if(process.env.JAWSDB_URL){
+   connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
 const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
